d042d29f9afbab13115bfcd22d4fa11b0a014f23
Joshua Sunil Mathew <119957696+joshuasunil10@users.noreply.github.com>
2024-04-19 04:11:31 +0100

    + comments

--- FileMatch.java
+++ FileMatch.java
@@ -1,18 +1,33 @@
+/* OBJECT ORIENTED PROGRAMMING ASSIGNMENT 
+ * 
+ * AUTHOR : JOSHUA SUNIL MATHEW C22419706
+ * 
+ * File Match - shows a matched file, and its corresponding strength of that match
+ * 
+ * */
+
+// imports
 import java.io.File;
 
+// representation of a matched file
 class FileMatch {
+	// the file itself
     private File file;
+    // strength
     private int matchCount;
 
+    // constructor
     public FileMatch(File file, double matchStrength) {
         this.file = file;
         this.matchCount = (int) matchStrength;
     }
 
+    // getter to retrieve the file
     public File getFile() {
         return file;
     }
 
+    // getter to get match strength
     public int getMatchCount() {
         return matchCount;
     }
